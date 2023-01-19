1dd8a325b2da46cd3d419847c70ecd42b39b683f
Abby <abby.e.smith.85@gmail.com>
2023-01-19 13:37:13 -0700

    commit tree 1f20ab3e9ebd9871520ce2053e5b8aa2e9efc464

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+//test 1
+const cats = ["Milo", "Otis", "Garfield"];
+
+//test 2
+function destructivelyAppendCat(){
+    return cats.push("Ralph");
+}
+
+//test 3
+function destructivelyPrependCat(){
+    return cats.unshift("Bob");
+}
+
+//test 4
+function destructivelyRemoveLastCat(){
+    return cats.pop(0);
+}
+
+//test 5
+function destructivelyRemoveFirstCat(){
+    return cats.shift(0);
+}
+
+//test 6
+function appendCat(){
+    return [...cats, "Broom"]
+}
+
+//test 7
+function prependCat(){
+    return ["Arnold", ...cats]
+}
+
+//test 8
+function removeLastCat(){
+    return cats.slice(0, cats.length - 1);
+}
+
+//test 9
+function removeFirstCat(){
+    return cats.slice(1)
+}
